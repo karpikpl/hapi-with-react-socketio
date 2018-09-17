e3d3094515263a8df2a12cb9d730e21695244eb8
Piotr Karpala <karpik.pl@gmail.com>
2018-09-17 10:08:28 -0400

    comment for sleep in test :(
    Signed-off-by: Piotr Karpala <karpik.pl@gmail.com>

--- client/src/VersionInfo.test.js
+++ client/src/VersionInfo.test.js
@@ -16,12 +16,12 @@ it('gets version from the backend', async () => {
     // act
     const div = document.createElement('div');
     ReactDOM.render(<VersionInfo />, div);
-    // wait a little bit
+    // wait a little bit so that response from the mock is processed before component is unmounted
     await new Promise(resolve => setTimeout(resolve, 100));
-    //ReactDOM.unmountComponentAtNode(div);
 
     // cleanup
     mock.reset();
+    ReactDOM.unmountComponentAtNode(div);
     // assert
     expect(mock.done()).toBeTruthy();
 });
@@ -33,12 +33,12 @@ it('displays error when backend call fails', async () => {
     // act
     const div = document.createElement('div');
     ReactDOM.render(<VersionInfo />, div);
-    // wait a little bit
+    // wait a little bit so that response from the mock is processed before component is unmounted
     await new Promise(resolve => setTimeout(resolve, 100));
-    //ReactDOM.unmountComponentAtNode(div);
 
     // cleanup
     mock.reset();
+    ReactDOM.unmountComponentAtNode(div);
 
     // assert
     expect(mock.done()).toBeTruthy();

1f6ec5b0197b72668dcd287ec74ccba9ba4d2aac
Piotr Karpala <karpik.pl@gmail.com>
2018-09-16 22:09:23 -0400

    test with nock
    Signed-off-by: Piotr Karpala <karpik.pl@gmail.com>

--- client/src/VersionInfo.test.js
+++ client/src/VersionInfo.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import VersionInfo from './VersionInfo';
+import Nock from 'nock';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<VersionInfo />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('gets version from the backend', async () => {
+    // arrange
+    const mock = Nock(/localhost/).get('/api/version')
+    .reply(200, 'some version')
+
+    // act
+    const div = document.createElement('div');
+    ReactDOM.render(<VersionInfo />, div);
+    // wait a little bit
+    await new Promise(resolve => setTimeout(resolve, 100));
+    ReactDOM.unmountComponentAtNode(div);
+
+    // assert
+    expect(mock.isDone()).toBeTruthy();
 });
